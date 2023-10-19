a3af617df1ec260a3529d58eeb384396e359be1e
Bruno <brunomonteiromestanza@gmail.com>
2023-10-19 20:32:30 -0300

    aula sobre estado

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,23 @@
+import { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 import './App.css';
-import { useState } from 'react';
 
 const theme = {
   main: '#c2410c',
 };
 
-interface StudentProps {
-  name: string;
-  registrationNumber: number;
-}
-
 export function App() {
-  const [students, setStudents] = useState<StudentProps[]>([]);
+  const [counter, setCounter] = useState(0);
 
-  function handleLoadStudents() {
-    setStudents([
-      {
-        name: 'Victor Italo',
-        registrationNumber: 25,
-      },
-      {
-        name: 'Jessica Sobreira',
-        registrationNumber: 13,
-      },
-      {
-        name: 'Bruna Fonseca',
-        registrationNumber: 1,
-      },
-      {
-        name: 'Rodrigo Minuzzo',
-        registrationNumber: 19,
-      },
-    ]);
+  function handleIncreaseCounter() {
+    setCounter(counter + 1)
   }
 
   return (
     <ThemeProvider theme={theme}>
       <div>
-        {students.length === 0 && <p>Informações não carregadas!</p>}
-        {students.map((student) => {
-          return (
-            <div key={student.registrationNumber}>
-              <p>Estudante: {student.name}</p>
-              <p>Matricula: {student.registrationNumber}</p>
-            </div>
-          );
-        })}
-        <button onClick={handleLoadStudents}>Carregar...</button>
+        <p>Contador: {counter}</p>
+        <button onClick={handleIncreaseCounter}>Aumentar</button>
       </div>
     </ThemeProvider>
   );
